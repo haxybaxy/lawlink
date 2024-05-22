4b27e0669a0ca7a623792209e9f75e5974c8ef16
haxy <zaidsaheb4@gmail.com>
2024-05-22 13:12:07 +0200

    fixed routing

--- lawlink/src/App.jsx
+++ lawlink/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Route, Routes, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './index';
 import LawyerListings from './pages/lawyerlistings';
 import Support from './pages/support';
@@ -9,13 +9,13 @@ import './index.css';
 function App() {
   return (
     <Router>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/lawyerlistings" element={<LawyerListings />} />
-            <Route path="/support" element={<Support />} />
-          </Routes>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/lawyerlistings" element={<LawyerListings />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
